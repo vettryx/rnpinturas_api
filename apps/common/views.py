312d626982ebde0a17b71761afb369cd487e0a95
André Ventura <ti@asventura.com.br>
2026-06-14 20:23:51 -0300

    refatorar: adicionar campo de descrição ao contexto da CommonTemplateView

--- apps/common/views.py
+++ apps/common/views.py
@@ -278,10 +278,12 @@ class CommonTemplateView(LoginRequiredMixin, TemplateView):
     """
 
     title = ""
+    description = ""
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = self.title
+        context["description"] = self.description
         return context
 
 
